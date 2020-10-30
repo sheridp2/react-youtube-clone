a4a1b3753b14960422c414364ba1cdaf220a6d4b
Patrick Sheriddan <sheridp2@gmail.com>
2020-10-29 19:12:10 -0700

    youtube api added to project

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const KEY = 'AIzaSyCarZIH488LJw2xGp0KuC_YaCZW5_nmDoY'
+const KEY = ''
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
