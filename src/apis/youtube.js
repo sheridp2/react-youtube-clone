7c4f26bb00363d78fd1f9d48922c476548ae3b08
Patrick Sheriddan <sheridp2@gmail.com>
2020-10-30 14:26:14 -0700

    render default term and first video in array

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const KEY = ''
+const KEY = 'AIzaSyDaoF1l1cGtncMwYKQ8U_w_b3rtUtxWa84'
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
